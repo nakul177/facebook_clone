52fe4d1d0799afd370b22e490dd9bdb60c2198b8
nakul177 <96129513+nakul177@users.noreply.github.com>
2022-06-08 10:12:54 +0530

    css pending only

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css';
 import { Header } from './Components/Header/Header';
 import { Posts } from './Components/Posts/Posts';
-import { Gif } from './Components/Gif/Gif';
+
 
 function App() {
   return (
